d577bbd4bf486bf0b969690408c665a92cac1dfb
Ana Luisa <analuisaaugustob@gmail.com>
2024-12-17 16:35:18 -0300

    style: Alterações de revisão de código.

--- js/app.js
+++ js/app.js
@@ -10,12 +10,13 @@ navToggle.addEventListener("click", () => {
 // CRUD - gerenciamento de livros
 
 // adicionando um livro
-const form = document.querySelector(".book-form");
-const alert = document.querySelector(".alert");
+const form = document.querySelector(".book-form"); // formulário de livros
+const alert = document.querySelector(".alert"); // elemento de alerta para mensagens
 
 form.addEventListener("submit", function (e) {
-  e.preventDefault();
+  e.preventDefault(); // impede o envio padrão do formulário
 
+  // obtendo os valores dos campos do formulário
   const title = document.getElementById("title").value;
   const author = document.getElementById("author").value;
   const pages = document.getElementById("pages").value;
@@ -23,25 +24,33 @@ form.addEventListener("submit", function (e) {
   const year = document.getElementById("year").value;
   const description = document.getElementById("description").value;
 
+  // criando um objeto livro com os dados do formulário
   const book = { title, author, pages, genre, year, description };
 
+  // recupera os livros armazenados no localStorage ou um array vazio, caso não existam
   const books = JSON.parse(localStorage.getItem("books")) || [];
 
+  // adiciona o novo livro ao array
   books.push(book);
 
+  // salva o array de livros de volta no localStorage
   localStorage.setItem("books", JSON.stringify(books));
 
+  // reseta os campos do formulário
   form.reset();
 
+  // exibe um alerta de sucesso
   displayAlert("Livro adicionado com sucesso", "success");
 });
 
-// função para aparecer o alerta ao usuário
+// função para exibir o alerta ao usuário
 function displayAlert(text, action) {
   alert.textContent = text; // define o texto do alerta
-  alert.classList.add(`alert-${action}`); // aplica estilo ao alerta
+  alert.classList.add(`alert-${action}`); // aplica o estilo ao alerta (sucesso ou erro)
+  
+  // remove o texto e o estilo do alerta após 2 segundos
   setTimeout(function () {
-    alert.textContent = ""; // remove o texto após 1 segundo
+    alert.textContent = ""; // remove o texto
     alert.classList.remove(`alert-${action}`); // remove o estilo
   }, 2000);
 }
